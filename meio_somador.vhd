39b1264a1c1819e860863758b95e338d0a21bf07
desenista668 <desenhista668@gamil.com>
2026-02-24 22:25:44 -0300

    Meio-Somador

--- meio_somador.vhd
+++ meio_somador.vhd
@@ -3,14 +3,15 @@ USE ieee.std_logic_1164.all;
 
 ENTITY meio_somador IS
 PORT (
- A,B: IN STD_LOGIC_VECTOR(3 DOWNTO 0);
- S, Cout: OUT STD_LOGIC_VECTOR(6 DOWNTO 0)
+ A, B: IN STD_LOGIC;
+ S, Cout: OUT STD_LOGIC
 );
 END ENTITY;
 ARCHITECTURE circuit OF meio_somador IS
 BEGIN
- 
+
  S <= A XOR B;
- Cout <= A AND B;
+ Cout <= A AND B; 
+ 
  
 END ARCHITECTURE;
